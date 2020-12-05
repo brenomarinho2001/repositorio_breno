9ab91c9284e788760d8ebb267cbc602fd8e0e8d9
brenomarinho2001 <74988159+brenomarinho2001@users.noreply.github.com>
2020-12-05 19:09:55 -0300

    Update calango.java

--- Projeto 02/calango.java
+++ Projeto 02/calango.java
@@ -1,5 +1,7 @@
 package projeto_calango;
 
+import java.util.Scanner;
+
 public class calango {
 	int vida;
 	int energia;
@@ -21,13 +23,13 @@ public class calango {
 		}
 		
 		if(qi > 50 && qi < 200) {
-			System.out.println("AINDA N√O … SUFICIENTE AAA mais QI!!");
+			System.out.println("AINDA N√ÉO √â SUFICIENTE AAA mais QI!!");
 			qi+=100;
 			return;
 		}
 		
 		if(qi > 200) {
-			System.out.println("AGORA SIM HAHAHAHA O PODER EST¡ NAS MINHAS M√OS");
+			System.out.println("AGORA SIM HAHAHAHA O PODER EST√Å NAS MINHAS M√ÉOS");
 			qi+=100;
 			return;
 		}
@@ -39,13 +41,13 @@ public class calango {
 		if(qi < 50)
 		{
 			vida -= 1;
-		System.out.println("EU N√O ACREDITO,PERDI POR QUE ESTOU BURRO AINDA! estou morrendo..");
+		System.out.println("EU N√ÉO ACREDITO,PERDI POR QUE ESTOU BURRO AINDA! estou morrendo..");
 		}
 		
 		if(qi > 50)
 		{
 			vida += 1;
-		System.out.println("MINHA VIDA EST¡ SE PROLONGANDO HAHAHAHAH esse È o poder.");
+		System.out.println("MINHA VIDA EST√Å SE PROLONGANDO HAHAHAHAH esse √© o poder.");
 		}
 		
 		
@@ -68,7 +70,7 @@ public class calango {
 			System.out.println("VIVEREI PARA SEMPRE HAHAHA...");
 		}
 		else {
-			System.out.println("ainda n„o tenho um organismo evoluido,preciso ver para sempre");
+			System.out.println("ainda n√£o tenho um organismo evoluido,preciso ver para sempre");
 		}
 	}
 	
@@ -81,6 +83,19 @@ public class calango {
 	 public static void main(String[] args) { 
 		 calango genio = new calango(3,4,10);
 		
+		 Scanner leitor = new Scanner(System.in);
+		
+		 genio.vida = leitor.nextInt();
+		 
+		 genio.energia = leitor.nextInt();
+		 
+		 genio.qi = leitor.nextInt();
+		 
+		 
+		 
+		 
+		 
+		 leitor.close();
 		 
 		 genio.briga();
 		 genio.sabedoria();
