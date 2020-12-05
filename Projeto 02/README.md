62b78839892c05cb360b4c27f2327bc8b5a7e5f9
brenomarinho2001 <brenomarinho2001@gmail.com>
2020-12-05 18:54:20 -0300

    Oi

---
+++ Projeto 02/README.md
@@ -0,0 +1,28 @@
+
+//--------------------------------CONTEXTO--------------------------------------------------------------------------
+
+Bom dia professor,então,sobre java e dificuldades,eu estava bem confuso em relação a orientação a objeto,por mais que eu já
+tive umas experiencias em relação a programação de jogos e web(gamemaker para jogos,e mais pra frente quero estudar a 
+engine unity,e comecei um pouco de java script no semestre passado e acabei aprendendo um pouquinho da parte web e as 
+linguagens de marcação html e css) mas era algo novo pra mim,porém,acompanhando as aulas e vendo os vídeos do guanabara 
+eu consegui entender um pouco melhor os conceitos.
+
+//------------------------------ Me animei com a ideia do calango----------------------------------------
+
+Eu até fiquei animado com a ideia do calango que fiz um pequeno joguinho em um dia ultilizando a engine gamemaker,aonde 
+ele pode brigar,perder o rabo,e correr.
+
+link do jogo:
+https://breno-marinho-1.itch.io/simulador-de-calango-breno
+
+eu curto muito tentar transformas coisas dos códigos em algo visual,me diverte bastante. 
+
+//--------------------------------Sobre o projeto 02 -----------------------------------------------------
+
+Sobre o projeto java eu resolvi criar o objeto calango e suas atribuições são relacionadas com inte-
+gência e energia, aonde ele quer aumentar o seu Qi para ser o ser mais inteligênte do universo.
+
+Atributos : Energia,Qi,Vida.
+
+Então,depois de experimentar alguns dois meios dado pelo senhor nas aulas,eu conseguir transformar o calango no ser mais 
+inteligênte do universo aumentando seu qi,e seus pontos de vida ficaram tão alto que ele virou imortal,muito obrigado.
---
+++ Projeto 02/calango.java
@@ -0,0 +1,104 @@
+package projeto_calango;
+
+public class calango {
+	int vida;
+	int energia;
+	int qi;
+	
+	
+    calango(int vida,int energia,int qi){
+        this.energia = energia;
+        this.vida = vida;
+        this.qi = qi;
+    }
+	
+	
+	void sabedoria(){
+		if(qi > 5 && qi < 15) {
+			System.out.println("Estou burro,preciso de mais poder");
+			qi+=50;
+			return;
+		}
+		
+		if(qi > 50 && qi < 200) {
+			System.out.println("AINDA NÃO É SUFICIENTE AAA mais QI!!");
+			qi+=100;
+			return;
+		}
+		
+		if(qi > 200) {
+			System.out.println("AGORA SIM HAHAHAHA O PODER ESTÁ NAS MINHAS MÃOS");
+			qi+=100;
+			return;
+		}
+
+	}
+	
+	void briga() {
+		
+		if(qi < 50)
+		{
+			vida -= 1;
+		System.out.println("EU NÃO ACREDITO,PERDI POR QUE ESTOU BURRO AINDA! estou morrendo..");
+		}
+		
+		if(qi > 50)
+		{
+			vida += 1;
+		System.out.println("MINHA VIDA ESTÁ SE PROLONGANDO HAHAHAHAH esse é o poder.");
+		}
+		
+		
+	}
+	
+	void energia_fluida()
+	{
+		if(energia > 10) {
+			System.out.println("meu neuronios borbulham por causa da minha energia");
+		}
+		else
+		{
+			System.out.println(" estou um pouco cansado..");
+		}
+	}
+	
+	void eternidade()
+	{
+		if(vida > 5) {
+			System.out.println("VIVEREI PARA SEMPRE HAHAHA...");
+		}
+		else {
+			System.out.println("ainda não tenho um organismo evoluido,preciso ver para sempre");
+		}
+	}
+	
+	
+	public String toString() {
+        return "calango : energia:" + energia + " vida:" + vida + " qi:" + qi;
+    }
+	
+	
+	 public static void main(String[] args) { 
+		 calango genio = new calango(3,4,10);
+		
+		 
+		 genio.briga();
+		 genio.sabedoria();
+		 genio.energia_fluida();
+		 System.out.println(genio);
+		 genio.sabedoria();
+		 genio.briga();
+		 genio.briga();
+		 genio.briga();
+		 genio.briga();
+		 System.out.println(genio);
+		 genio.eternidade();
+		 System.out.println(genio);
+	 }
+	
+	
+	
+	
+	
+
+}
